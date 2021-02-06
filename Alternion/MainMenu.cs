d8bf304e0d78c13dbc9f27b28eddaccbd5135cd0
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-06 12:23:52 +0000

    Moved main menu stuff to standalone class

---
+++ Alternion/MainMenu.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+using BWModLoader;
+using Steamworks;
+
+namespace Alternion
+{
+    [Mod]
+    public class MainMenuCL : MonoBehaviour
+    {
+        /// <summary>
+        /// Main menu character transform.
+        /// </summary>
+        static Transform menuCharacter;
+
+        public static MainMenuCL Instance;
+
+        /// <summary>
+        /// Sets the main menu weapon skin.
+        /// </summary>
+        static void setMainMenuWeaponSkin()
+        {
+            if (AlternionSettings.useWeaponSkins)
+            {
+                try
+                {
+                    string steamID = SteamUser.GetSteamID().m_SteamID.ToString();
+                    if (theGreatCacher.players.TryGetValue(steamID, out playerObject player))
+                    {
+                        var musket = GameObject.Find("wpn_standardMusket_LOD1");
+                        if (musket != null)
+                        {
+                            if (theGreatCacher.weaponSkins.TryGetValue("musket_" + player.musketSkinName, out Texture newTex))
+                            {
+                                musket.GetComponent<Renderer>().material.mainTexture = newTex;
+                            }
+                        }
+                        else
+                        {
+                            Logger.debugLog("Main menu musket not found.");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.debugLog(e.Message);
+                }
+            }
+
+            LoadingBar.updatePercentage(100, "Finished!");
+        }
+
+        /// <summary>
+        /// Sets the main menu badge.
+        /// </summary>
+        public static void setMainMenuBadge()
+        {
+
+            if (!AlternionSettings.useBadges)
+            {
+                LoadingBar.updatePercentage(95, "applying weapon skin");
+                setMainMenuWeaponSkin();
+                return;
+            }
+
+            //Only main menu that you will really see is the one intially started
+            //This doesn't work if you return to the main menu from a server
+            MainMenu mm = FindObjectOfType<MainMenu>();
+
+            try
+            {
+                string steamID = Steamworks.SteamUser.GetSteamID().ToString();
+                if (theGreatCacher.players.TryGetValue(steamID, out playerObject player))
+                {
+                    Logger.debugLog($"Got player {player.steamID} => {player.badgeName}");
+                    if (mm.menuBadge.texture.name != "tournamentWake1Badge" ^ (!AlternionSettings.showTWBadges & mm.menuBadge.texture.name == "tournamentWake1Badge"))
+                    {
+                        if (theGreatCacher.badges.TryGetValue(player.badgeName, out Texture newTex))
+                        {
+                            mm.menuBadge.texture = newTex;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Logger.debugLog("Failed to assign custom badge to a player:");
+                Logger.debugLog(e.Message);
+            }
+
+            LoadingBar.updatePercentage(95, "Applying weapon skin");
+            setMainMenuWeaponSkin();
+
+        }
+
+        /// <summary>
+        /// Sets the main menu character transform.
+        /// </summary>
+        static void setMenuCharacter()
+        {
+            // Find the musket object
+            var musket = GameObject.Find("wpn_standardMusket_LOD1");
+            if (musket != null)
+            {
+                // If it exists, then go to root and find the character model in the heirachy
+                Transform rootTransf = musket.transform.root;
+                foreach (Transform transform in rootTransf)
+                {
+                    if (transform.name == "default_character_rig")
+                    {
+                        // Save it for the rotating in Update()
+                        menuCharacter = transform;
+                        break;
+                    }
+                }
+            }
+        }
+
+        void Start()
+        {
+            //Rotate Character
+            setMenuCharacter();
+        }
+        void Update()
+        {
+            if (!óèïòòåææäêï.åìçæçìíäåóë.activeSelf && global::Input.GetMouseButton(1) && menuCharacter)
+            {
+                // If it has been found
+                // Rotation code copied from CharacterCustomizationUI
+                menuCharacter.Rotate(Vector3.up, 1000f * Time.deltaTime * -global::Input.GetAxisRaw("Mouse X"));
+
+            }
+
+            if (Input.GetKeyUp("`"))
+            {
+                Logger.debugLog("v7.0");
+            }
+        }
+
+        [HarmonyPatch(typeof(MainMenu), "toggleKSBadge")]
+        static class toggleKSPatch
+        {
+            static void Postfix(MainMenu __instance, bool on)
+            {
+                if (AlternionSettings.useBadges)
+                {
+                    if (!on)
+                    {
+                        setMainMenuBadge();
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(MainMenu), "Start")]
+        static class mainMenuStuffPatch
+        {
+            static void Postfix(MainMenu __instance)
+            {
+                // Call these so that they set correctly again on returning to the main menu
+                setMainMenuBadge();
+                setMenuCharacter();
+            }
+        }
+    }
+}
