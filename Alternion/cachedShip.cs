fa9decd0bd8f9f0667c6f3da106fe56e166a24df
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-03-13 15:32:48 +0000

    Added int field for team num

--- Alternion/cachedShip.cs
+++ Alternion/cachedShip.cs
@@ -25,9 +25,13 @@ namespace Alternion
         /// </summary>
         public bool isNavy;
         /// <summary>
+        /// Check for if the ship is navy or pirate (navy = true, pirate = false)
+        /// </summary>
+        public int teamNum;
+        /// <summary>
         /// Ship flag renderer
         /// </summary>
-        public Renderer flag;
+        public List<Renderer> flags = new List<Renderer>();
         /// <summary>
         /// Stores all Secondary Sails.
         /// </summary>
