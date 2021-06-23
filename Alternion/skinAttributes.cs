b318b897036c9ecc5f3ced2a9ffe44ec75fbc12f
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-06-24 00:37:11 +0100

    Added initial template for skin attributes

---
+++ Alternion/skinAttributes.cs
@@ -0,0 +1,10 @@
+﻿namespace Alternion
+{
+    public class skinAttributes
+    {
+        public string weaponSkin;
+        public bool hasAlb;
+        public bool hasMet;
+        public bool hasNrm;
+    }
+}
