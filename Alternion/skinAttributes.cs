ce28a3df240ca6f9a27c2a9c9a33eb9c6d50bb16
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-08 23:38:53 +0100

    Removed sail skins attributes

--- Alternion/skinAttributes.cs
+++ Alternion/skinAttributes.cs
@@ -7,11 +7,4 @@
         public bool hasMet;
         public bool hasNrm;
     }
-    public class sailSkinAttributes
-    {
-        public string sailSkin;
-        public bool hasAlb;
-        public bool hasMet;
-    }
-
 }
