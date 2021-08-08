845de3c8f8f22c92a2252de807a83622f3ae5532
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-09 00:10:05 +0100

    Removed Birb stuff

--- Alternion/Mainmod.cs
+++ Alternion/Mainmod.cs
@@ -3166,46 +3166,5 @@ namespace Alternion
             }
         }
 
-#if DEBUG
-        /// <summary>
-        /// Handles disabling/enabling bird on winners
-        /// </summary>
-        /// <param name="__instance">Current Player's PlayerInfo</param> 
-        private IEnumerator handleBird(PlayerInfo __instance)
-        {
-            yield return new WaitForSeconds(.5f);
-            __instance.isTournyWinner = true;
-            Logger.logLow($"{!__instance.character.æïðèñìæêêñç}");
-            __instance.character.óððêäóäîçñè.SetActive(!__instance.character.æïðèñìæêêñç); // Only enable if not owner
-            Logger.logLow("IsBird");
-            if (__instance.character.æïðèñìæêêñç)
-            {
-                __instance.character.óððêäóäîçñè.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly; // Sets to only use shadows, so it still shows in FP
-                Logger.logLow("Removed from FP");
-            }
-            if (__instance.lobbyPlayer != null)
-            {
-                __instance.lobbyPlayer.ñéçåäçëñåæê = true;
-            }
-        }
-
-        /// <summary>
-        /// Harmony patch to disable bird in first person
-        /// </summary>
-        [HarmonyPatch(typeof(PlayerInfo), "setWinner")]
-        static class isWinnerPatch
-        {
-            static bool Prefix(PlayerInfo __instance, ïçîìäîóäìïæ.åéðñðçîîïêç info)
-            {
-                if (info.åéñëîíèðòçé)
-                {
-                    Instance.StartCoroutine(Instance.handleBird(__instance));
-                }
-
-                return false;
-            }
-        }
-#endif
-
     }
 }
