6ad03b7be65773d9c402ddcffee7b5870a4821b9
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-12 16:42:18 +0100

    Added intial attempt to fix ShipLOD.Update() null reference error

---
+++ Alternion/Fixes/shipLod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+
+namespace Alternion.Fixes
+{
+    class shipLod
+    {
+        [HarmonyPatch(typeof(ShipLOD), "Update")]
+        class shipLodUpdatePatch
+        {
+            static bool Prefix(ShipLOD __instance)
+            {
+                if (__instance.transform.parent == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
