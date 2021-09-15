00f2e0997407dc86e14bb8b52973ef08d282ba83
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-09-15 21:03:22 +0100

    Added test code to github for future referencing - KEY2345

---
+++ Alternion/Structs/type.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alternion.Structs
+{
+#if EXTRAS
+    public enum textureType
+    {
+        BADGE = 0,
+        MASK = 1,
+        SECONDARY_SAIL = 2,
+        MAIN_SAIL = 3,
+        CANNON = 4,
+        SWIVEL = 5,
+        MORTAR = 6,
+        FLAG_NAVY = 7,
+        FLAG_PIRATE = 8,
+    }
+#endif
+}
