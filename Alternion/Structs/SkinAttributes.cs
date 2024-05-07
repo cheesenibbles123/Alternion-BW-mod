9db0adc887d5df7366ca8994e0cd63de2a28f5a9
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2024-05-07 19:29:12 +0100

    Added optional bools for potential swivel models

--- Alternion/Structs/SkinAttributes.cs
+++ Alternion/Structs/SkinAttributes.cs
@@ -7,5 +7,9 @@
         public bool hasMet;
         public bool hasNrm;
         public bool hasMesh;
+
+        public bool? hasMeshTop;
+        public bool? hasMeshConnector;
+        public bool? hasMeshBase;
     }
 }
