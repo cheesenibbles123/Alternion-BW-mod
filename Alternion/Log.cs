b053b42dbc72f32d5228cbc7e1284af6cccb2036
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2024-05-07 19:27:35 +0100

    Added comments and improved logger class structure

--- Alternion/Log.cs
+++ Alternion/Log.cs
@@ -8,33 +8,38 @@ namespace Alternion
     
     public class Logger
     {
-        internal static class Log
+        /// <summary>
+        /// Prefix to be appended to the start of any messages sent from this instance
+        /// </summary>
+        private string TAG;
+
+        static readonly public ModLogger logger = new ModLogger("[Alternion]", ModLoader.LogPath + "\\Alternion.txt");
+
+        public Logger(string tag = "")
         {
-            static readonly public ModLogger logger = new ModLogger("[Alternion]", ModLoader.LogPath + "\\Alternion.txt");
+            TAG = tag;
         }
 
         /// <summary>
         /// Always logs, no matter the logging level.
         /// </summary>
-        /// <param name="message">Message to Log</param>
-        public static void debugLog(string message)
+        public void debugLog(string message)
         {
             //Just easier to type than Log.logger.Log
             //Will always log, so only use in try{} catch(Exception e) {} when absolutely needed
-            Log.logger.Log(message);
+            logger.Log(TAG + ": " + message);
         }
 
         /// <summary>
         /// Logs debug items at logging levels higher than 0.
         /// </summary>
-        /// <param name="message">Message to Log</param>
-        public static void logLow(string message)
+        public void logLow(string message)
         {
             //Just easier to type than Log.logger.Log
             // Also lets me just set logLevel to 0 if I dont want to deal with the spam.
             if (AlternionSettings.loggingLevel > 0)
             {
-                Log.logger.Log(message);
+                debugLog(TAG + ": " + message);
             }
         }
     }
