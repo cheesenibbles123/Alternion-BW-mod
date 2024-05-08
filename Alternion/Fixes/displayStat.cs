6f3d8b96704079c50a432001c91e2db293256dd4
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2024-05-08 22:15:02 +0100

    Updated logger

--- Alternion/Fixes/displayStat.cs
+++ Alternion/Fixes/displayStat.cs
@@ -4,21 +4,21 @@ using Steamworks;
 namespace Alternion.Fixes
 {
     // CURRENTLY INACTIVE AND AWAITING UPDATE
-#if EXTRAS
     class displayStat
     {
-        
+        /*
+        private static Logger logger = new Logger("[DisplayStat]");
         [HarmonyPatch(typeof(PlayerInfo), "setupRemotePlayer")]
         class displayStatPatch
         {
             static void Postfix(PlayerInfo __instance, string pName, int t, int s, int k, int d, int a, bool back, string dStat, int dMedal, int ks, float kd, short wins, float wl, short bm, short sm, short gm)
             {
-                Logger.debugLog("Got setup for " + pName);
-                Logger.debugLog($"Medal set to -{__instance.displayStat}-{dStat}- ");
-                Logger.debugLog($"Level set to -{__instance.displayStatMedal}-{dMedal}- ");
+                logger.debugLog("Got setup for " + pName);
+                logger.debugLog($"Medal set to -{__instance.displayStat}-{dStat}- ");
+                logger.debugLog($"Level set to -{__instance.displayStatMedal}-{dMedal}- ");
             }
         }
-
+        
         [HarmonyPatch(typeof(AccoladeItem), "ëîéæìêìëéæï")]
         class accoladeSetupPatch
         {
@@ -29,6 +29,6 @@ namespace Alternion.Fixes
                 Logger.debugLog($"Accolade Medal set to -{plrInf.displayStat}-{plrInf.displayStatMedal}- ");
             }
         }
+        */
     }
-#endif
 }
