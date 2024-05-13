ec54a35457ce54a234f00bc66ebb6db59c70a453
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2024-05-14 00:14:54 +0100

    Moved to array format as this allows for quicker load times when loading an existing ship

--- Alternion/SkinHandlers/flagHandler.cs
+++ Alternion/SkinHandlers/flagHandler.cs
@@ -4,6 +4,7 @@ using BWModLoader;
 using Harmony;
 using UnityEngine;
 using Alternion.Structs;
+using System.Reflection;
 
 namespace Alternion.SkinHandlers
 {
@@ -16,6 +17,7 @@ namespace Alternion.SkinHandlers
         public static flagHandler Instance;
         private static Logger logger = new Logger("[FlagHandler]");
         const float assignDelay = 4f; // Game uses 3, so lower than this can be inconsistent
+        int maxRuns = 20;
 
         void Awake()
         {
@@ -31,20 +33,37 @@ namespace Alternion.SkinHandlers
 
         private IEnumerator wasteTime(FlagSet __instance)
         {
-            yield return new WaitForSeconds(assignDelay);
-            Instance.StartCoroutine(Instance.setFlag(GameMode.getParentIndex(__instance.transform.root), __instance.GetComponent<Renderer>(), null, 0));
+            yield return new WaitForSeconds(.1f);
+            int index = -1;
+            int counter = 0;
+            cachedShip vessel = null;
+            while (counter < maxRuns)
+            {
+                index = GameMode.getParentIndex(__instance.transform.root);
+                if (index != -1)
+                {
+                    if (vessel == null) vessel = TheGreatCacher.Instance.getCachedShip(index.ToString());
+                    if (GameMode.Instance.teamCaptains[index])
+                    {
+                        Instance.StartCoroutine(Instance.setFlag(index, __instance.GetComponent<Renderer>(), null, assignDelay));
+                    }
+                    break;
+                }
+                counter++;
+                yield return new WaitForSeconds(.4f);
+            }
         }
         public IEnumerator setFlag(int index, Renderer renderer, Texture newTex = null, float delay = assignDelay)
         {
             yield return new WaitForSeconds(delay);
+
             bool isPirates = GameMode.Instance.teamFactions[index] == "Pirates";
             string steamID = GameMode.Instance.teamCaptains[index].steamID.ToString();
-
             cachedShip vessel = TheGreatCacher.Instance.getCachedShip(index.ToString());
+            vessel.flags.Add(renderer);
             if (!vessel.isInitialized)
             {
                 vessel.isNavy = !isPirates;
-                vessel.flag = renderer;
                 vessel.isInitialized = true;
             }
 
@@ -52,7 +71,7 @@ namespace Alternion.SkinHandlers
             {
                 if (newTex)
                 {
-                    setFlagTexture(vessel, newTex);
+                    setFlagTextures(vessel, newTex);
                     vessel.hasChangedFlag = true;
                     yield break;
                 }
@@ -60,11 +79,14 @@ namespace Alternion.SkinHandlers
                 if (TheGreatCacher.Instance.players.TryGetValue(steamID, out playerObject player) &&
                     TheGreatCacher.Instance.flags.TryGetValue(isPirates ? player.flagPirateSkinName : player.flagNavySkinName, out newTex))
                 {
-                    setFlagTexture(vessel, newTex);
+                    setFlagTextures(vessel, newTex);
                     vessel.hasChangedFlag = true;
                     yield break;
                 }
             }
+            logger.debugLog("Resetting tex!");
+            logger.debugLog($"Result: {TheGreatCacher.Instance.players.TryGetValue(steamID, out playerObject playerLog)} {TheGreatCacher.Instance.flags.TryGetValue(isPirates ? playerLog.flagPirateSkinName : playerLog.flagNavySkinName, out newTex)}");
+
             resetFlag(vessel);
         }
 
@@ -73,22 +95,23 @@ namespace Alternion.SkinHandlers
         {
             if (vessel.hasChangedFlag)
             {
-                setFlagTexture(vessel, vessel.isNavy ? TheGreatCacher.Instance.navyFlag : TheGreatCacher.Instance.pirateFlag, true);
+                setFlagTextures(vessel, vessel.isNavy ? TheGreatCacher.Instance.navyFlag : TheGreatCacher.Instance.pirateFlag, true);
             }
         }
 
-        public static void setFlagTexture(cachedShip vessel, Texture newTexture, bool isChangingToDefault = false)
+        public static void setFlagTextures(cachedShip vessel, Texture newTexture, bool isChangingToDefault = false)
         {
-            vessel.flag.material.mainTexture = newTexture;
+            vessel.flags.ForEach((flag) => flag.material.mainTexture = newTexture);
             vessel.hasChangedFlag = !isChangingToDefault;
         }
 
         [HarmonyPatch(typeof(FlagSet), "OnEnable")]
         class FlatSetPatch
         {
-            static void Postfix(FlagSet __instance)
+            static bool Prefix(FlagSet __instance)
             {
                 Instance.StartCoroutine(Instance.wasteTime(__instance));
+                return false;
             }
         }
     }
